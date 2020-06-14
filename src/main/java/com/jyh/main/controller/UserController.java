8aba71b500255055ce72f03a463f4a7260eede20
jhonyhe <33723378+jhonyhe@users.noreply.github.com>
2020-06-14 17:15:35 +0800

    修改Usercontroller的业务逻辑 使用了Redis进行缓存

--- src/main/java/com/jyh/main/controller/UserController.java
+++ src/main/java/com/jyh/main/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jyh.main.dao.UserDao;
 import com.jyh.main.modle.User;
 import com.jyh.main.util.IpUtil;
+import com.jyh.main.util.RedisUtil;
  
 @RestController
 public class UserController {
@@ -26,24 +27,30 @@ public class UserController {
     }
  
     @GetMapping(value="/login")
-    @PostMapping(value="/login")
     public String findTestByName(String id,String passWord,HttpServletRequest request){
     	System.out.println("id = " + id + " passWord = "+passWord);
     	String str= userDao.findTestByName(id, passWord);
+    	RedisUtil.StringOps.set("user", str);
+        System.out.println("In redis user is "+ RedisUtil.StringOps.get("user"));
     	String ipAddress =IpUtil.getIpAddr(request);
     	System.out.println(ipAddress);
-        System.out.println("login is "+str);
+        System.out.println("In java user is "+str);
         return str;
     }
  
     
     @GetMapping(value="/updateUser")
     public String updateTest(User user){
-    	return userDao.updateTest(user);
+    	String str= userDao.updateTest(user);
+    	RedisUtil.StringOps.set("user", str);
+        System.out.println("In redis user is " + RedisUtil.StringOps.get("user"));
+    	return str;
     }
  
     @GetMapping(value="/deleteUser")
-    public void deleteTestById(){
-        userDao.deleteTestById("11");
+    public void deleteTestById(String id){
+        userDao.deleteTestById(id);
+        RedisUtil.StringOps.set("user", new String());
+        System.out.println("In redis user is " + RedisUtil.StringOps.get("user"));
     }
 }

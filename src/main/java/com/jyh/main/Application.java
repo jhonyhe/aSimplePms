b50705a296cc70e7e068ef1c1d1608abc0ad1538
jhonyhe <33723378+jhonyhe@users.noreply.github.com>
2020-10-04 15:13:11 +0800

    修改了启动SpringBoot的主类
    使得其注入了context这个bean，后续能处理这个bean里面的各种信息

--- src/main/java/com/jyh/main/Application.java
+++ src/main/java/com/jyh/main/Application.java
@@ -2,10 +2,12 @@ package com.jyh.main;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
  
 @SpringBootApplication
 public class Application {
- 
+	public static ConfigurableApplicationContext context;
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
